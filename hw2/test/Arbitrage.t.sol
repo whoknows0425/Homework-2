e7a6f5a467f83b68dc32faafe43057bb4fb1951c
whoknows0425 <114318900+whoknows0425@users.noreply.github.com>
2024-04-11 22:40:12 +0800

    Update Arbitrage.t.sol

--- hw2/test/Arbitrage.t.sol
+++ hw2/test/Arbitrage.t.sol
@@ -79,6 +79,14 @@ contract Arbitrage is Test {
         /**
          * Please add your solution below
          */
+        address[] memory path = new address[](5);
+        path[0] = address(tokenB);
+        path[1] = address(tokenA);
+        path[2] = address(tokenD);
+        path[3] = address(tokenC);
+        path[4] = address(tokenB);
+        
+        router.swapExactTokensForTokens(5 ether, 0, path, address(arbitrager), block.timestamp + 10);
         /**
          * Please add your solution above
          */
